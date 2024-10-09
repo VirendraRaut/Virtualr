9856c25eca8e34f2ea02f0f1c6b0bafe2c517790
VirendraRaut <virendraraut1306@gmail.com>
2024-10-09 22:32:29 +0530

    css to logo image

--- src/Components/Navbar.jsx
+++ src/Components/Navbar.jsx
@@ -6,7 +6,7 @@ const Navbar = () => {
       <div className="container px-4 mx-auto relative text-sm">
         <div className="flex justify-center items-center">
           <div className="flex items-center flex-shrink-0">
-            <img src={logo} alt="" />
+            <img className="w-10 h-10 mr-2" src={logo} alt="" />
           </div>
         </div>
       </div>
