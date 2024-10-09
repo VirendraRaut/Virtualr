93b64677b85327dbdccbf4405bb923d3f8559c61
VirendraRaut <virendraraut1306@gmail.com>
2024-10-09 22:28:23 +0530

    created two div elements

--- src/Components/Navbar.jsx
+++ src/Components/Navbar.jsx
@@ -1,6 +1,10 @@
 const Navbar = () => {
   return (
-    <nav className="sticky top-0 z-50 py-3 backdrop-blur-lg border-b border-neutral-700/80"></nav>
+    <nav className="sticky top-0 z-50 py-3 backdrop-blur-lg border-b border-neutral-700/80">
+      <div className="container px-4 mx-auto relative text-sm">
+        <div className="flex justify-center items-center"></div>
+      </div>
+    </nav>
   );
 };
 
