3ea427b18132f848738d97de695cf0d4a195dc99
VirendraRaut <virendraraut1306@gmail.com>
2024-10-09 22:35:50 +0530

    first element of navbar created successfully

--- src/Components/Navbar.jsx
+++ src/Components/Navbar.jsx
@@ -7,6 +7,7 @@ const Navbar = () => {
         <div className="flex justify-center items-center">
           <div className="flex items-center flex-shrink-0">
             <img className="w-10 h-10 mr-2" src={logo} alt="" />
+            <span className="text-xl tracking-tight">VirtualR</span>
           </div>
         </div>
       </div>
