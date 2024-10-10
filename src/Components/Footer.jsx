808c5a7a135f49528e89f5fa296a603b00585882
VirendraRaut <virendraraut1306@gmail.com>
2024-10-11 01:58:07 +0530

    removing div element

--- src/Components/Footer.jsx
+++ src/Components/Footer.jsx
@@ -1,22 +1,20 @@
-import { resourcesLinks, platformLinks, communityLinks } from "../constants"
+import { resourcesLinks, platformLinks, communityLinks } from "../constants";
 
 const Footer = () => {
   return (
-    <div>
-      <footer className="mt-20 border-t py-10 border-neutral-700">
+    <footer className="mt-20 border-t py-10 border-neutral-700">
       <div className="grid grid-cols-2 lg:grid-cols-3 gap-4">
         <h3 className="text-md font-semibold mb-4">Resources</h3>
         <ul>
-          {resourcesLinks.map((link,index)=>(
+          {resourcesLinks.map((link, index) => (
             <li key={index}>
               <a href={link.href}>{link.text}</a>
             </li>
           ))}
         </ul>
       </div>
-      </footer>
-    </div>
-  )
-}
+    </footer>
+  );
+};
 
-export default Footer
+export default Footer;
