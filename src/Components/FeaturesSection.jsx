11480f3ce22f714fc13ed1e36156e521ed429920
VirendraRaut <virendraraut1306@gmail.com>
2024-10-10 18:09:54 +0530

    added features with its description

--- src/Components/FeaturesSection.jsx
+++ src/Components/FeaturesSection.jsx
@@ -22,7 +22,10 @@ const FeaturesSection = () => {
               <div className="flex mx-6 h-10 w-10 p-2 bg-neutral-900 text-orange-700 justify-center items-center rounded-full">
                 {features.icon}
               </div>
-              <h2 className="mt-1 mb-6 text-xl">{features.text}</h2>
+              <div>
+                <h2 className="mt-1 mb-6 text-xl">{features.text}</h2>
+                <p>{features.description}</p>
+              </div>
             </div>
           </div>
         ))}
