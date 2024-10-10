31a441670f755a57cdc6aee556da6e02dc17dd18
VirendraRaut <virendraraut1306@gmail.com>
2024-10-10 19:02:52 +0530

    added title

--- src/Components/Workflow.jsx
+++ src/Components/Workflow.jsx
@@ -22,6 +22,9 @@ const Workflow = () => {
               <div className="text-green-400 mx-6 bg-neutral-900 h-10 w-10 p-2 justify-center items-center rounded-full">
                 <CheckCircle2 />
               </div>
+              <div>
+                <h5 className="mt-1 mb-2 text-xl">{items.title}</h5>
+              </div>
             </div>
           ))}
         </div>
