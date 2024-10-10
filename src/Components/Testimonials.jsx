53faf1b12a2e778463c12c8e36915288f561070a
VirendraRaut <virendraraut1306@gmail.com>
2024-10-10 20:10:05 +0530

    text center

--- src/Components/Testimonials.jsx
+++ src/Components/Testimonials.jsx
@@ -1,11 +1,13 @@
-import { testimonials } from "../constants"
+import { testimonials } from "../constants";
 
 const Testmonials = () => {
   return (
     <div className="mt-20 tracking-wide">
-      <h2 className="text-3xl sm:text-5xl lg:text-6xl">What people are saying</h2>
+      <h2 className="text-3xl sm:text-5xl lg:text-6xl text-center">
+        What people are saying
+      </h2>
     </div>
-  )
-}
+  );
+};
 
-export default Testmonials
+export default Testmonials;
