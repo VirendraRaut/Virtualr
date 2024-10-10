ebb8a41d6932529d8f999f48ade95aee94253a9d
VirendraRaut <virendraraut1306@gmail.com>
2024-10-10 20:43:53 +0530

    added image

--- src/Components/Testimonials.jsx
+++ src/Components/Testimonials.jsx
@@ -11,6 +11,13 @@ const Testmonials = () => {
           <div key={index} className="w-full sm:w-1/2 lg:w-1/3 px-4 py-2">
             <div className="bg-neutral rounded-md p-6 text-md border border-neutral-800 font-thin">
               <p className="">{testimonial.text}</p>
+              <div className="flex mt-8 items-start">
+                <img
+                  className="w-12 h-12 mr-6 border border-neutral-300 rounded-full"
+                  src={testimonial.image}
+                  alt=""
+                />
+              </div>
             </div>
           </div>
         ))}
