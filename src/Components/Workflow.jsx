ac9db42c49bc5c31a5e71998864d659b4a2f668d
VirendraRaut <virendraraut1306@gmail.com>
2024-10-10 19:00:06 +0530

    checkCircleIcon

--- src/Components/Workflow.jsx
+++ src/Components/Workflow.jsx
@@ -19,7 +19,9 @@ const Workflow = () => {
         <div className="pt-12 w-full lg:w-1/2">
           {checklistItems.map((items, index) => (
             <div key={index} className="flex mb-12">
-              <div className="text-green-400 mx-6 bg-neutral-900 h-10 w-10 p-2 justify-center items-center rounded-full"></div>
+              <div className="text-green-400 mx-6 bg-neutral-900 h-10 w-10 p-2 justify-center items-center rounded-full">
+                <CheckCircle2 />
+              </div>
             </div>
           ))}
         </div>
