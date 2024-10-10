6cbf66cfa04f8f8c26ad100835d94fbdbb2274db
VirendraRaut <virendraraut1306@gmail.com>
2024-10-11 01:56:53 +0530

    change in css of heading tag

--- src/Components/Footer.jsx
+++ src/Components/Footer.jsx
@@ -5,7 +5,7 @@ const Footer = () => {
     <div>
       <footer className="mt-20 border-t py-10 border-neutral-700">
       <div className="grid grid-cols-2 lg:grid-cols-3 gap-4">
-        <h3 className="text-md mb-4 font-semibold">Resources</h3>
+        <h3 className="text-md font-semibold mb-4">Resources</h3>
         <ul>
           {resourcesLinks.map((link,index)=>(
             <li key={index}>
