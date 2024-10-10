637df8b3193c0f304f9600c14bc8dc0288487283
VirendraRaut <virendraraut1306@gmail.com>
2024-10-10 18:01:34 +0530

    adding div

--- src/Components/FeaturesSection.jsx
+++ src/Components/FeaturesSection.jsx
@@ -1,3 +1,5 @@
+import { features } from "../constants/index";
+
 const FeaturesSection = () => {
   return (
     <div className="relative mt-20 border-b border-neutral-800 min-h-[800px]">
@@ -12,6 +14,15 @@ const FeaturesSection = () => {
             your code
           </span>
         </h2>
+      </div>
+      <div className="flex flex-wrap mt-10 lg:mt-20">
+        {features.map((features, index) => (
+          <div key={index} className="w-full sm:1/2 lg:w-1/3 ">
+            <div className="flex">
+              <div className="flex mx-6 h-10 w-10 p-2 bg-neutral-900 text-orange-700 justify-center items-center rounded-full"></div>
+            </div>
+          </div>
+        ))}
       </div>
     </div>
   );
