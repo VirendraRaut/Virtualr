f03ca68788957ded0dadb180b81af4e4c524d2a1
VirendraRaut <virendraraut1306@gmail.com>
2024-10-10 18:39:48 +0530

    spelling mistake

--- src/Components/Workflow.jsx
+++ src/Components/Workflow.jsx
@@ -5,7 +5,7 @@ const Workflow = () => {
   return (
     <div className="mt-20">
       <h2 className="text-3xl sm:text-5xl lg:text-6xl text-center mt-6 tracking-wide">
-        Accerelate your coding workflow
+        Accelerate your coding workflow
       </h2>
     </div>
   );
