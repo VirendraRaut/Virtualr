52c2084b31221805d4a1d3b5bb745b8c6ce53a00
VirendraRaut <virendraraut1306@gmail.com>
2024-10-10 19:39:06 +0530

    css changed

--- src/Components/Pricing.jsx
+++ src/Components/Pricing.jsx
@@ -11,7 +11,14 @@ const Pricing = () => {
         {pricingOptions.map((options, index) => (
           <div key={index} className="w-full sm:w-1/2 lg:w-1/3 p-2">
             <div className="p-10 border border-neutral-700 rounded-xl">
-              <p className="text-4xl mb-8">{options.title}</p>
+              <p className="text-4xl mb-8">
+                {options.title}
+                {options.title === "Pro" && (
+                  <span className="bg-gradient-to-r from-orange-500 to bg-red-500 text-transparent bg-clip-text text-xl mb-4 ml-2">
+                    (Most Popular)
+                  </span>
+                )}
+              </p>
             </div>
           </div>
         ))}
