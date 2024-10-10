b3ece0a22ef49bd3edfeb17c88871f6baaebfe15
VirendraRaut <virendraraut1306@gmail.com>
2024-10-10 18:45:09 +0530

    css for last two words of span tag

--- src/Components/Workflow.jsx
+++ src/Components/Workflow.jsx
@@ -5,7 +5,11 @@ const Workflow = () => {
   return (
     <div className="mt-20">
       <h2 className="text-3xl sm:text-5xl lg:text-6xl text-center mt-6 tracking-wide">
-        Accelerate your coding workflow
+        Accelerate your{" "}
+        <span className="bg-gradient-to-r from-orange-500 to bg-orange-800 text-transparent bg-clip-text">
+          {" "}
+          coding workflow
+        </span>
       </h2>
     </div>
   );
