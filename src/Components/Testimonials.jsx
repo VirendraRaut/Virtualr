ebbdb93f10073fd3eed328154cf74a39fe44d33a
VirendraRaut <virendraraut1306@gmail.com>
2024-10-10 20:36:21 +0530

    error solved

--- src/Components/Testimonials.jsx
+++ src/Components/Testimonials.jsx
@@ -7,10 +7,10 @@ const Testmonials = () => {
         What people are saying
       </h2>
       <div className="flex flex-wrap justify-center">
-        {testimonials.map((index, testimonials) => (
+        {testimonials.map((testimonial, index) => (
           <div key={index} className="w-full sm:w-1/2 lg:w-1/3 px-4 py-2">
             <div className="bg-neutral rounded-md p-6 text-md border border-neutral-800 font-thin">
-              <p className="">{testimonials.text}</p>
+              <p className="">{testimonial.text}</p>
             </div>
           </div>
         ))}
