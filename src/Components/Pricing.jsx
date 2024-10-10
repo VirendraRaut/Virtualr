1c8a06198d194aac265a1e753b4fbda5ad2673bb
VirendraRaut <virendraraut1306@gmail.com>
2024-10-10 19:26:47 +0530

    added heading of pricing

--- src/Components/Pricing.jsx
+++ src/Components/Pricing.jsx
@@ -1,11 +1,14 @@
-
+import { CheckCircle2 } from "lucide-react";
+import { pricingOptions } from "../constants";
 
 const Pricing = () => {
   return (
-    <div>
-      
+    <div className="mt-20 ">
+      <h2 className="text-3xl sm:text-5xl lg:text-6xl text-center my-8 tracking-wide">
+        Pricing
+      </h2>
     </div>
-  )
-}
+  );
+};
 
-export default Pricing
+export default Pricing;
