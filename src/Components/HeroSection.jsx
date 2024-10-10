9e77e5c855dd9911e4caaf83daa4e30c566c6966
VirendraRaut <virendraraut1306@gmail.com>
2024-10-10 15:42:57 +0530

    an space needed

--- src/Components/HeroSection.jsx
+++ src/Components/HeroSection.jsx
@@ -5,7 +5,7 @@ const HeroSection = () => {
   return (
     <div className="flex flex-col mt-6 lg:mt-20 items-center">
       <h1 className="text-4xl sm:text-6xl lg:text-7xl text-center tracking-wide">
-        VirtualR build tools
+        VirtualR build tools &nbsp;
         <span className="bg-gradient-to-r from-orange-500 to-orange-800 text-transparent bg-clip-text">
           for developers
         </span>
