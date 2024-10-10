f173bb8c3f890414b7c94fc11e0081794edc2a48
VirendraRaut <virendraraut1306@gmail.com>
2024-10-10 19:34:22 +0530

    css to title

--- src/Components/Pricing.jsx
+++ src/Components/Pricing.jsx
@@ -11,7 +11,7 @@ const Pricing = () => {
         {pricingOptions.map((options, index) => (
           <div key={index} className="w-full sm:w-1/2 lg:w-1/3 p-2">
             <div className="p-10 border border-neutral-700 rounded-xl">
-              <p className="">{options.title}</p>
+              <p className="text-4xl mb-8">{options.title}</p>
             </div>
           </div>
         ))}
