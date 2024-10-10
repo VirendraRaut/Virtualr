4b359879a477d5f96c55b524dedb34e8494a6388
VirendraRaut <virendraraut1306@gmail.com>
2024-10-10 15:38:49 +0530

    heading to herosection

--- src/Components/HeroSection.jsx
+++ src/Components/HeroSection.jsx
@@ -1,10 +1,14 @@
+import video1 from "../assets/video1.mp4";
+import video2 from "../assets/video2.mp4";
 
 const HeroSection = () => {
   return (
-    <div>
-      HeroSection
+    <div className="flex flex-col mt-6 lg:mt-20 items-center">
+      <h1 className="text-4xl sm:text-6xl lg:text-7xl text-center tracking-wide">
+        VirtualR build tools for developers
+      </h1>
     </div>
-  )
-}
+  );
+};
 
-export default HeroSection
+export default HeroSection;
