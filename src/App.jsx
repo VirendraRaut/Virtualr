7b79e3c8ad824ba8670c0208778078a841c0b741
VirendraRaut <virendraraut1306@gmail.com>
2024-10-10 15:32:52 +0530

    created a div and display herosection in it

--- src/App.jsx
+++ src/App.jsx
@@ -1,14 +1,15 @@
-import HeroSection from "./Components/HeroSection"
-import Navbar from "./Components/Navbar"
-
+import HeroSection from "./Components/HeroSection";
+import Navbar from "./Components/Navbar";
 
 const App = () => {
   return (
     <>
-      <Navbar/>
-      <HeroSection/>
+      <Navbar />
+      <div className="max-w-7xl mx-auto pt-20 px-6">
+        <HeroSection />
+      </div>
     </>
-  )
-}
+  );
+};
 
-export default App
+export default App;
