9e72a7809124d7f4f43c797632b564bea5d61c30
VirendraRaut <virendraraut1306@gmail.com>
2024-10-11 01:53:11 +0530

    importing links data from constant folder

--- src/Components/Footer.jsx
+++ src/Components/Footer.jsx
@@ -1,9 +1,16 @@
-
+import { resourcesLinks, platformLinks, communityLinks } from "../constants"
 
 const Footer = () => {
   return (
     <div>
-      
+      <footer className="mt-20 border-t py-10 border-neutral-700">
+      <div className="grid grid-cols-2 lg:grid-cols-3 gap-4">
+        <h3 className="text-md mb-4 font-semibold">Resources</h3>
+        <ul>
+          
+        </ul>
+      </div>
+      </footer>
     </div>
   )
 }
