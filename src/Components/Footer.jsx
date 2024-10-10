ddb8bbe05eb9daf8a9bb30818f3b7d9760261741
VirendraRaut <virendraraut1306@gmail.com>
2024-10-11 01:55:19 +0530

    added resources link

--- src/Components/Footer.jsx
+++ src/Components/Footer.jsx
@@ -7,7 +7,11 @@ const Footer = () => {
       <div className="grid grid-cols-2 lg:grid-cols-3 gap-4">
         <h3 className="text-md mb-4 font-semibold">Resources</h3>
         <ul>
-          
+          {resourcesLinks.map((link,index)=>(
+            <li key={index}>
+              <a href={link.href}>{link.text}</a>
+            </li>
+          ))}
         </ul>
       </div>
       </footer>
