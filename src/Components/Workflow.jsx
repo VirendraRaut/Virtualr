4c0521dbffcc1d303e7df71b95a503b484c5e66c
VirendraRaut <virendraraut1306@gmail.com>
2024-10-10 18:37:46 +0530

    changes

--- src/Components/Workflow.jsx
+++ src/Components/Workflow.jsx
@@ -4,7 +4,7 @@ import codeImg from "../assets/code.jpg";
 const Workflow = () => {
   return (
     <div className="mt-20">
-      <h2 className="text-3xl sm:text-5xl lg:text-6xl text-center tracking-wide mt-6">
+      <h2 className="text-3xl sm:text-5xl lg:text-6xl text-center mt-6 tracking-wide">
         Accerelate your coding workflow
       </h2>
     </div>
