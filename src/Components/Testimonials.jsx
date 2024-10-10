adc51d023a7ffb800e51a97486a2dcc82492a877
VirendraRaut <virendraraut1306@gmail.com>
2024-10-10 20:08:53 +0530

    added heading to it

--- src/Components/Testimonials.jsx
+++ src/Components/Testimonials.jsx
@@ -3,7 +3,7 @@ import { testimonials } from "../constants"
 const Testmonials = () => {
   return (
     <div className="mt-20 tracking-wide">
-      
+      <h2 className="text-3xl sm:text-5xl lg:text-6xl">What people are saying</h2>
     </div>
   )
 }
