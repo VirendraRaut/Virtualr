1d9812574e56c5f6ae049510776e52e861b0c61d
VirendraRaut <virendraraut1306@gmail.com>
2024-10-10 17:51:50 +0530

    bg change for last two words of h2 tag

--- src/Components/FeaturesSection.jsx
+++ src/Components/FeaturesSection.jsx
@@ -6,7 +6,10 @@ const FeaturesSection = () => {
           Features
         </span>
         <h2 className="text-3xl sm:text-5xl lg:text-6xl mt-10 lg:mt-20 tracking-wide">
-          Easily build your code
+          Easily build
+          <span className="bg-gradient-to-r from-orange-500 to bg-orange-800">
+            your code
+          </span>
         </h2>
       </div>
     </div>
